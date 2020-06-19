8e118b062c1b580d2829f3011b4defbac659002a
KouheiHata <50469638+KouheiHata@users.noreply.github.com>
2020-06-19 09:57:00 +0900

    内覧会の来場予約ページ

---
+++ page-showing.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Template Name: 来場予約
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php if (wp_is_mobile()) { //モバイルデバイスでアクセスされた場合 ?>
+<div class="key_top"><img src="<?php echo get_template_directory_uri(); ?>/images/keyword_smart.png" alt="人｜地元とのつながりを第一に"></div>
+<?php } else { //それ以外でアクセスされた場合 ?>
+<div class="key_top"><img src="<?php echo get_template_directory_uri(); ?>/images/keyword.png" alt="人｜地元とのつながりを第一に"></div>
+<?php } ?>
+<?php echo do_shortcode('[metaslider id="903"]'); ?>
+
+<div class="container access-top">
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h4 class="top-title">来場予約<span class="h6"> -reserve-</span></h4>
+        </div>
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<?php echo do_shortcode('[contact-form-7 id="1176" title="内覧会予約"]'); ?>
+        </div>
+   </div>
+</div>
+
+<?php get_footer(); ?>
